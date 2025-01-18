4147880c673987f538b52f3f2aa60ff28cc28942
Rbitah <bsc-35-21@unima.ac.mw>
2025-01-18 19:33:43 +0200

    make phoneNumber and phoneForPayment fields nullable in User entity

--- src/user.entity.ts
+++ src/user.entity.ts
@@ -5,10 +5,10 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ nullable: true })
   phoneNumber: string;
 
-  @Column()
+  @Column({ nullable: true })
   phoneForPayment: string;
 
   @Column({ nullable: true })
